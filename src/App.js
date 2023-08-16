08cdcb157797d4850f835d5d85c6f2c6e19b06e8
Kunal Patil <kp327313@gmail.com>
2023-08-16 15:41:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {AiOutlineSearch} from 'react-icons/ai'
+
 import './App.css';
 
+const api = {
+  key : process.env.REACT_APP_API_KEY,
+  base: 'https://api.openweathermap.org/data/2.5/',
+}
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&appid=${api.key}`)
+    .then(res => res.json())
+    .then(result => {
+      // console.log(result)
+      // console.log(weather.message);
+      if (result.message === "city not found") {
+        window.alert("City not found");
+      } else {
+        setWeather(result);
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Weather App</h1>
+      <div className='search'>
+        <input
+        type='text'
+        placeholder='Enter City/town'
+        onChange={(e) => setSearch(e.target.value)}
+        />
+        <AiOutlineSearch onClick={searchPressed} className='icon'/>
+      </div>
+
+      <div>
+        <img src='./icon.png' className='img_icon'/>
+      </div>
+
+      {typeof weather.main !== "undefined" ? (
+          <div className='weather_info'>
+            {/* Temperature Celsius  */}
+            <p className='temp'>{weather.main.temp}°C</p>
+
+            {/* Location  */}
+            <p className='city'>{weather.name}</p>
+
+            {/* Condition (Sunny ) */}
+            <p>{weather.weather[0].main}</p>
+            <p>({weather.weather[0].description})</p>
+          </div>
+        ) : (
+          ""
+        )}
+        
       </header>
     </div>
   );
